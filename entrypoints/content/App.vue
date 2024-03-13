2650b5104a17a6e99e70851a1b07b36dd24cc3ce
Tobias Thiele <thiele231@web.de>
2024-03-13 22:00:11 +0100

    ✨ Add menu icon to the navigation bar
    - Added a new SVG menu icon to the navigation bar.
    - The menu item now has an ID for easier identification.
    - Implemented a function to check and update the menu item based on the width of the navigation element.
    - The observer and interval are properly cleared when unmounting.
    
    This commit adds a new menu icon, improves menu item handling, and ensures proper cleanup.

--- entrypoints/content/App.vue
+++ entrypoints/content/App.vue
@@ -9,10 +9,13 @@ import PageConfig from "@/components/PageConfig.vue";
 import { waitForElement } from "@/utils";
 import { AutodartsToolsConfig, defaultConfig } from "@/utils/storage";
 
+const menuIcon = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" viewBox=\"0 0 24 24\"><path fill=\"currentColor\" d=\"M8.8 21H5q-.825 0-1.412-.587T3 19v-3.8q1.2 0 2.1-.762T6 12.5q0-1.175-.9-1.937T3 9.8V6q0-.825.588-1.412T5 4h4q0-1.05.725-1.775T11.5 1.5q1.05 0 1.775.725T14 4h4q.825 0 1.413.588T20 6v4q1.05 0 1.775.725T22.5 12.5q0 1.05-.725 1.775T20 15v4q0 .825-.587 1.413T18 21h-3.8q0-1.25-.787-2.125T11.5 18q-1.125 0-1.912.875T8.8 21\"/></svg>";
+
 let observer = new MutationObserver(() => {});
 const currentUrl = ref();
 const configVisible = ref(false);
 const isConfigPage = ref(true);
+const navigationCheckInterval = ref();
 
 watch(currentUrl, async (newURL, oldURL) => {
   const config = await AutodartsToolsConfig.getValue();
@@ -75,10 +78,11 @@ onMounted(async () => {
   const menuItem = menuItemTemplate.cloneNode(true) as HTMLElement;
   menuItem.removeAttribute("href");
   const withText = menuItem.innerText.length > 0;
+  menuItem.id = "autodarts-tools-menu-item";
   menuItem.innerHTML = "";
   menuItem.style.cursor = "pointer";
 
-  menuItem.innerHTML += "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" viewBox=\"0 0 24 24\"><path fill=\"currentColor\" d=\"M8.8 21H5q-.825 0-1.412-.587T3 19v-3.8q1.2 0 2.1-.762T6 12.5q0-1.175-.9-1.937T3 9.8V6q0-.825.588-1.412T5 4h4q0-1.05.725-1.775T11.5 1.5q1.05 0 1.775.725T14 4h4q.825 0 1.413.588T20 6v4q1.05 0 1.775.725T22.5 12.5q0 1.05-.725 1.775T20 15v4q0 .825-.587 1.413T18 21h-3.8q0-1.25-.787-2.125T11.5 18q-1.125 0-1.912.875T8.8 21\"/></svg>";
+  menuItem.innerHTML += menuIcon;
 
   if (withText) {
     menuItem.innerHTML += "Tools";
@@ -95,6 +99,13 @@ onMounted(async () => {
   });
 
   menu.appendChild(menuItem);
+
+  navigationCheckInterval.value = setInterval(checkNavigation, 1000);
+});
+
+onBeforeUnmount(() => {
+  observer.disconnect();
+  clearInterval(navigationCheckInterval.value);
 });
 
 function startObserver() {
@@ -114,5 +125,31 @@ function startObserver() {
     }
   });
   observer.observe(targetNode, { childList: true, subtree: true });
+}
+
+function checkNavigation() {
+  const navigationElement = document.querySelector("#root > div > div");
+
+  // get width of navigationElement
+  const width = navigationElement?.getBoundingClientRect().width;
+
+  // if width < 170, find element with id "autodarts-tools-menu-item" and remove it's text but not the svg in it
+  if (width && width < 170) {
+    const menuItem = document.getElementById("autodarts-tools-menu-item");
+    if (menuItem) {
+      menuItem.innerHTML = menuIcon;
+    }
+  } else if (width && width > 200) {
+    const menuItem = document.getElementById("autodarts-tools-menu-item");
+    if (menuItem) {
+      menuItem.innerHTML = menuIcon;
+    }
+  } else {
+    const menuItem = document.getElementById("autodarts-tools-menu-item");
+    if (menuItem) {
+      menuItem.innerHTML = `${menuIcon} Tools`;
+      menuItem.querySelector("svg")!.style.marginRight = "0.5rem";
+    }
+  }
 }
 </script>
