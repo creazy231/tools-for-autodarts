eace93687a55332684ea54669943c8d5f4003551
Sebastian Budde <moin@budde.io>
2024-03-27 01:34:56 +0100

    Fix: playSound

--- utils/playSound.ts
+++ utils/playSound.ts
@@ -5,29 +5,29 @@ soundEffect1.autoplay = true;
 soundEffect2.autoplay = true;
 soundEffect3.autoplay = true;
 
-const isSoundsEnabled = (await AutodartsToolsConfig.getValue()).sounds.enabled;
-
-export function playSound1(fileName) {
+export async function playSound1(fileName) {
+  const isSoundsEnabled = (await AutodartsToolsConfig.getValue()).sounds.enabled;
   if (!isSoundsEnabled || !fileName) return;
-  if (!fileName) return;
   // console.log("fileName1", fileName);
   soundEffect1.src = fileName;
 }
 
-export function playSound2(fileName) {
+export async function playSound2(fileName) {
+  const isSoundsEnabled = (await AutodartsToolsConfig.getValue()).sounds.enabled;
   if (!isSoundsEnabled || !fileName) return;
   // console.log('fileName2', fileName);
   soundEffect2.src = fileName;
 }
 
-export function playSound3(fileName) {
+export async function playSound3(fileName) {
+  const isSoundsEnabled = (await AutodartsToolsConfig.getValue()).sounds.enabled;
   if (!isSoundsEnabled || !fileName) return;
-  if (!fileName) return;
   // console.log('fileName3', fileName);
   soundEffect3.src = fileName;
 }
 
 export function playPointsSound(callerServerUrl: string, callerFileExt: string, turnPoints?: string) {
   if (!turnPoints) return;
-  if (callerServerUrl?.length) soundEffect1.src = callerServerUrl + turnPoints + callerFileExt;
+  if (callerServerUrl?.length) playSound1(callerServerUrl + turnPoints + callerFileExt);
+  // if (callerServerUrl?.length) soundEffect1.src = callerServerUrl + turnPoints + callerFileExt;
 }
