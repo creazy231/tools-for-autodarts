21b7e08c015b8827b29564f7069d218569e38324
Sebastian Budde <moin@budde.io>
2024-04-01 19:11:36 +0200

    Fix: winner sounds / caller

--- entrypoints/match.content/soundsWinner.ts
+++ entrypoints/match.content/soundsWinner.ts
@@ -14,11 +14,12 @@ export async function soundsWinner() {
   const winnerPlayerName = (winnerPlayerCard?.querySelector(".ad-ext-player-name") as HTMLElement)?.innerText;
 
   const isSoundsEnabled = (await AutodartsToolsConfig.getValue()).sounds.enabled;
+  const isCallerEnabled = (await AutodartsToolsConfig.getValue()).caller.enabled;
 
   const callerActive = (await AutodartsToolsCallerConfig.getValue()).caller.filter(caller => caller.isActive)[0];
   const soundsConfig = await AutodartsToolsSoundsConfig.getValue();
 
-  if (!isSoundsEnabled) return;
+  if (!isCallerEnabled && !isSoundsEnabled) return;
 
   let callerServerUrl = callerActive?.url || "";
   if (callerServerUrl.at(-1) !== "/") callerServerUrl += "/";
@@ -27,17 +28,19 @@ export async function soundsWinner() {
     const buttons = [ ...document.querySelectorAll(".chakra-button") as NodeList ];
     const isLegWinner = buttons.findIndex(button => (button as HTMLElement).innerText === "Next Leg") !== -1;
     if (isLegWinner) {
-      if (callerServerUrl.length) playPointsSound(callerServerUrl, ".mp3", "gameshot", 3);
+      if (isCallerEnabled && callerServerUrl.length) playPointsSound(callerServerUrl, ".mp3", "gameshot", 3);
     } else {
       const isMatchWinner = buttons.findIndex(button => (button as HTMLElement).innerText === "Finish") !== -1;
       if (isMatchWinner) {
-        if (callerServerUrl.length) playPointsSound(callerServerUrl, ".mp3", "gameshot and the match", 3);
+        if (isCallerEnabled && callerServerUrl.length) playPointsSound(callerServerUrl, ".mp3", "gameshot and the match", 3);
 
-        setTimeout(() => {
-          const winnerSoundIndex = soundsConfig.winner.findIndex(winner => winner.name.toLowerCase() === winnerPlayerName?.toLowerCase());
-          const soundIndex = winnerSoundIndex && soundsConfig.winner[winnerSoundIndex]?.info ? winnerSoundIndex : 0;
-          playSound("winner", 2, soundIndex);
-        }, 1000);
+        if (isSoundsEnabled) {
+          setTimeout(() => {
+            const winnerSoundIndex = soundsConfig.winner.findIndex(winner => winner.name.toLowerCase() === winnerPlayerName?.toLowerCase());
+            const soundIndex = winnerSoundIndex && soundsConfig.winner[winnerSoundIndex]?.info ? winnerSoundIndex : 0;
+            playSound("winner", 2, soundIndex);
+          }, 1000);
+        }
       }
     }
   }, waitForSumCallingIsOver);
