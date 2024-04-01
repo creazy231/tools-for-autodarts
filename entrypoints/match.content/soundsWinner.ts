64161733f41aa71288cd39ce79f3764a66eb94f5
Sebastian Budde <moin@budde.io>
2024-04-01 18:58:15 +0200

    Feature: add upload of sound files

--- entrypoints/match.content/soundsWinner.ts
+++ entrypoints/match.content/soundsWinner.ts
@@ -1,7 +1,7 @@
 import { AutodartsToolsConfig, AutodartsToolsMatchStatus } from "@/utils/storage";
 import { AutodartsToolsCallerConfig } from "@/utils/callerStorage";
 import { AutodartsToolsSoundsConfig } from "@/utils/soundsStorage";
-import { playSound2, playSound3 } from "@/utils/playSound";
+import { playPointsSound, playSound } from "@/utils/playSound";
 
 export async function soundsWinner() {
   const matchStatus = (await AutodartsToolsMatchStatus.getValue());
@@ -16,7 +16,7 @@ export async function soundsWinner() {
   const isSoundsEnabled = (await AutodartsToolsConfig.getValue()).sounds.enabled;
 
   const callerActive = (await AutodartsToolsCallerConfig.getValue()).caller.filter(caller => caller.isActive)[0];
-  const soundConfig = await AutodartsToolsSoundsConfig.getValue();
+  const soundsConfig = await AutodartsToolsSoundsConfig.getValue();
 
   if (!isSoundsEnabled) return;
 
@@ -25,22 +25,20 @@ export async function soundsWinner() {
 
   setTimeout(() => {
     const buttons = [ ...document.querySelectorAll(".chakra-button") as NodeList ];
-    buttons.some((button) => {
-      // --- Leg finished ---
-      if ((button as HTMLElement).innerText === "Next Leg") {
-        if (callerServerUrl.length) playSound3(`${callerServerUrl}gameshot.mp3`);
-        return true;
-      }
-      // --- Match finished ---
-      if ((button as HTMLElement).innerText === "Finish") {
-        if (callerServerUrl.length) playSound3(`${callerServerUrl}gameshot and the match.mp3`);
+    const isLegWinner = buttons.findIndex(button => (button as HTMLElement).innerText === "Next Leg") !== -1;
+    if (isLegWinner) {
+      if (callerServerUrl.length) playPointsSound(callerServerUrl, ".mp3", "gameshot", 3);
+    } else {
+      const isMatchWinner = buttons.findIndex(button => (button as HTMLElement).innerText === "Finish") !== -1;
+      if (isMatchWinner) {
+        if (callerServerUrl.length) playPointsSound(callerServerUrl, ".mp3", "gameshot and the match", 3);
+
         setTimeout(() => {
-          const winnerSound = soundConfig.winner.find(winner => winner.name.toLowerCase() === winnerPlayerName?.toLowerCase())?.url;
-          playSound2(winnerSound || soundConfig.winner[0].url);
+          const winnerSoundIndex = soundsConfig.winner.findIndex(winner => winner.name.toLowerCase() === winnerPlayerName?.toLowerCase());
+          const soundIndex = winnerSoundIndex && soundsConfig.winner[winnerSoundIndex]?.info ? winnerSoundIndex : 0;
+          playSound("winner", 2, soundIndex);
         }, 1000);
-        return true;
       }
-      return false;
-    });
+    }
   }, waitForSumCallingIsOver);
 }
