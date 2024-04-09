fe23e45de9b9602b1467d7df0522f680b544474d
Sebastian Budde <moin@budde.io>
2024-04-10 00:34:01 +0200

    Fix: add error catch

--- entrypoints/match.content/scoreSmaller.ts
+++ entrypoints/match.content/scoreSmaller.ts
@@ -5,21 +5,25 @@ export async function scoreSmaller() {
   const { inactiveSmall } = await AutodartsToolsConfig.getValue();
   if (!inactiveSmall.enabled) return;
 
-  addStyles(`
+  try {
+    addStyles(`
         .adp_points-small { font-size: 3em!important; }
         `);
 
-  const activePlayerCardPointsEl = document.querySelector(".ad-ext-player-active .ad-ext-player-score") || document.querySelector(".ad-ext-player-winner .ad-ext-player-score");
-  const inactivePlayerCardPointsElArr = [ ...document.querySelectorAll(".ad-ext-player-inactive .ad-ext-player-score") ];
+    const activePlayerCardPointsEl = document.querySelector(".ad-ext-player-active .ad-ext-player-score") || document.querySelector(".ad-ext-player-winner .ad-ext-player-score");
+    const inactivePlayerCardPointsElArr = [ ...document.querySelectorAll(".ad-ext-player-inactive .ad-ext-player-score") ];
 
-  // if Bull-off
-  const bustedPlayerElArr = document.querySelectorAll(".ad-ext-player-busted .ad-ext-player-score");
-  if (bustedPlayerElArr.length) {
-    inactivePlayerCardPointsElArr.push(...bustedPlayerElArr);
-  }
+    // if Bull-off
+    const bustedPlayerElArr = document.querySelectorAll(".ad-ext-player-busted .ad-ext-player-score");
+    if (bustedPlayerElArr.length) {
+      inactivePlayerCardPointsElArr.push(...bustedPlayerElArr);
+    }
 
-  if (inactivePlayerCardPointsElArr.length && activePlayerCardPointsEl) {
-    activePlayerCardPointsEl.classList.remove("adp_points-small");
-    [ ...inactivePlayerCardPointsElArr ].forEach(el => el.classList.add("adp_points-small"));
+    if (inactivePlayerCardPointsElArr.length && activePlayerCardPointsEl) {
+      activePlayerCardPointsEl.classList.remove("adp_points-small");
+      [ ...inactivePlayerCardPointsElArr ].forEach(el => el.classList.add("adp_points-small"));
+    }
+  } catch (e) {
+    console.error("Autodarts Tools: Set score smaller - Error: ", e);
   }
 }
--- entrypoints/match.content/soundsWinner.ts
+++ entrypoints/match.content/soundsWinner.ts
@@ -5,54 +5,58 @@ import { playPointsSound, playSound } from "@/utils/playSound";
 import { getWinnerPlayerCard } from "@/utils/getElements";
 
 export async function soundsWinner() {
-  const matchStatus = (await AutodartsToolsMatchStatus.getValue());
+  try {
+    const matchStatus = (await AutodartsToolsMatchStatus.getValue());
 
-  const throwPointsArr = matchStatus.throws;
-  const turnPoints = matchStatus.turnPoints;
+    const throwPointsArr = matchStatus.throws;
+    const turnPoints = matchStatus.turnPoints;
 
-  const isSoundsEnabled = (await AutodartsToolsConfig.getValue()).sounds.enabled;
-  const isCallerEnabled = (await AutodartsToolsConfig.getValue()).caller.enabled;
+    const isSoundsEnabled = (await AutodartsToolsConfig.getValue()).sounds.enabled;
+    const isCallerEnabled = (await AutodartsToolsConfig.getValue()).caller.enabled;
 
-  // if turnPoints is 100 or more, then sum calling time is 3500ms, otherwise 2500ms, because of longer time to call 100+ points
-  const sumCallingTime = turnPoints ? (Number.parseInt(turnPoints) >= 100 ? 3500 : 2500) : 0;
+    // if turnPoints is 100 or more, then sum calling time is 3500ms, otherwise 2500ms, because of longer time to call 100+ points
+    const sumCallingTime = turnPoints ? (Number.parseInt(turnPoints) >= 100 ? 3500 : 2500) : 0;
 
-  const waitForSumCallingIsOver = (isCallerEnabled && throwPointsArr.length === 3) ? sumCallingTime : 0;
+    const waitForSumCallingIsOver = (isCallerEnabled && throwPointsArr.length === 3) ? sumCallingTime : 0;
 
-  const winnerPlayerCard = getWinnerPlayerCard();
-  const winnerPlayerName = (winnerPlayerCard?.querySelector(".ad-ext-player-name") as HTMLElement)?.innerText;
+    const winnerPlayerCard = getWinnerPlayerCard();
+    const winnerPlayerName = (winnerPlayerCard?.querySelector(".ad-ext-player-name") as HTMLElement)?.innerText;
 
-  const callerActive = (await AutodartsToolsCallerConfig.getValue()).caller.filter(caller => caller.isActive)[0];
-  const soundsConfig = await AutodartsToolsSoundsConfig.getValue();
+    const callerActive = (await AutodartsToolsCallerConfig.getValue()).caller.filter(caller => caller.isActive)[0];
+    const soundsConfig = await AutodartsToolsSoundsConfig.getValue();
 
-  if (!isCallerEnabled && !isSoundsEnabled) return;
+    if (!isCallerEnabled && !isSoundsEnabled) return;
 
-  const isWinnerSoundOnLegWin = soundsConfig.winnerSoundOnLegWin;
+    const isWinnerSoundOnLegWin = soundsConfig.winnerSoundOnLegWin;
 
-  let callerServerUrl = callerActive?.url || "";
-  if (callerServerUrl.at(-1) !== "/") callerServerUrl += "/";
+    let callerServerUrl = callerActive?.url || "";
+    if (callerServerUrl.at(-1) !== "/") callerServerUrl += "/";
 
-  const playWinnerSound = () => {
-    if (isSoundsEnabled) {
-      setTimeout(() => {
-        const winnerSoundIndex = soundsConfig.winner.findIndex(winner => winner.name.toLowerCase() === winnerPlayerName?.toLowerCase());
-        const soundIndex = winnerSoundIndex && soundsConfig.winner[winnerSoundIndex]?.info ? winnerSoundIndex : 0;
-        playSound("winner", 2, soundIndex);
-      }, 1000);
-    }
-  };
-
-  setTimeout(() => {
-    const buttons = [ ...document.querySelectorAll(".chakra-button") as NodeList ];
-    const isLegWinner = buttons.findIndex(button => (button as HTMLElement).innerText === "Next Leg") !== -1;
-    if (isLegWinner) {
-      if (isCallerEnabled && callerServerUrl.length) playPointsSound(callerServerUrl, ".mp3", "gameshot", 3);
-      isWinnerSoundOnLegWin && playWinnerSound();
-    } else {
-      const isMatchWinner = buttons.findIndex(button => (button as HTMLElement).innerText === "Finish") !== -1;
-      if (isMatchWinner) {
-        if (isCallerEnabled && callerServerUrl.length) playPointsSound(callerServerUrl, ".mp3", "gameshot and the match", 3);
-        playWinnerSound();
+    const playWinnerSound = () => {
+      if (isSoundsEnabled) {
+        setTimeout(() => {
+          const winnerSoundIndex = soundsConfig.winner.findIndex(winner => winner.name.toLowerCase() === winnerPlayerName?.toLowerCase());
+          const soundIndex = winnerSoundIndex && soundsConfig.winner[winnerSoundIndex]?.info ? winnerSoundIndex : 0;
+          playSound("winner", 2, soundIndex);
+        }, 1000);
       }
-    }
-  }, waitForSumCallingIsOver);
+    };
+
+    setTimeout(() => {
+      const buttons = [ ...document.querySelectorAll(".chakra-button") as NodeList ];
+      const isLegWinner = buttons.findIndex(button => (button as HTMLElement).innerText === "Next Leg") !== -1;
+      if (isLegWinner) {
+        if (isCallerEnabled && callerServerUrl.length) playPointsSound(callerServerUrl, ".mp3", "gameshot", 3);
+        isWinnerSoundOnLegWin && playWinnerSound();
+      } else {
+        const isMatchWinner = buttons.findIndex(button => (button as HTMLElement).innerText === "Finish") !== -1;
+        if (isMatchWinner) {
+          if (isCallerEnabled && callerServerUrl.length) playPointsSound(callerServerUrl, ".mp3", "gameshot and the match", 3);
+          playWinnerSound();
+        }
+      }
+    }, waitForSumCallingIsOver);
+  } catch (e) {
+    console.error("Autodarts Tools: Set winner sounds - Error: ", e);
+  }
 }
