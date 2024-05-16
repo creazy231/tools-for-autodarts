bd243b0034819fde6a74593f9e30e4de6798e63c
Tobias Thiele <thiele231@web.de>
2024-05-16 03:04:26 +0200

    🚀 Update package version to 1.3.11
    Bump up the package version from 1.3.10 to 1.3.11 in the package.json file, improving Autodarts Tools on autodarts.io gaming platform.

--- entrypoints/match.content/nextPlayerOnTakeOutStuck.ts
+++ entrypoints/match.content/nextPlayerOnTakeOutStuck.ts
@@ -1,23 +1,68 @@
 import type { TBoardStatus } from "@/utils/storage";
 import { AutodartsToolsBoardStatus, AutodartsToolsConfig } from "@/utils/storage";
 import { BoardStatus } from "@/utils/types";
-import { getNextBtn } from "@/utils/getElements";
+import { waitForElementWithTextContent } from "@/utils";
+
+// Create a map to store event listeners
+const eventListenersMap = new Map();
+
+// Create a wrapper around addEventListener
+// @ts-expect-error
+Document.prototype.realAddEventListener = Document.prototype.addEventListener;
+Document.prototype.addEventListener = function (eventName, callback) {
+// @ts-expect-error
+  this.realAddEventListener(eventName, callback);
+
+  if (!eventListenersMap.has(eventName)) {
+    eventListenersMap.set(eventName, []);
+  }
+
+  eventListenersMap.get(eventName).push(callback);
+};
+
+// Create a function to check if an event listener has been defined
+function hasEventListener(eventName, callback) {
+  const listeners = eventListenersMap.get(eventName);
+  return listeners && listeners.includes(callback);
+}
 
 export async function nextPlayerOnTakeOutStuck() {
   try {
-    const { nextPlayerOnTakeOutStuck } = await AutodartsToolsConfig.getValue();
-    if (!nextPlayerOnTakeOutStuck.enabled) return;
+    const configValue = await AutodartsToolsConfig.getValue();
+    if (!configValue || !configValue.nextPlayerOnTakeOutStuck || !configValue.nextPlayerOnTakeOutStuck.enabled) return;
+
+    // check if element with id "ad-ext_next-leg-active" is already added to body. if yes, return
+    if (document.getElementById("ad-ext_next-leg-active")) return;
+
+    // add element with id "ad-ext_next-leg-active" to body
+    const nextLegActiveEl = document.createElement("div");
+    nextLegActiveEl.id = "ad-ext_next-leg-active";
+    nextLegActiveEl.style.display = "none";
+    document.body.appendChild(nextLegActiveEl);
 
     let takeOutTimout: NodeJS.Timeout;
 
+    function remove() {
+      const element = document.getElementById("ad-ext_next-leg-text");
+      element?.remove();
+      if (takeOutTimout) clearInterval(takeOutTimout);
+    }
+
+    if (!hasEventListener("click", remove)) {
+      document.addEventListener("click", remove);
+    }
+
     AutodartsToolsBoardStatus.watch(async (boardStatus: TBoardStatus) => {
-      takeOutTimout && clearInterval(takeOutTimout);
+      const nextBtnTextEl = document.getElementById("ad-ext_next-leg-text");
+      nextBtnTextEl?.remove();
+
+      if (takeOutTimout) clearInterval(takeOutTimout);
 
       if (boardStatus === BoardStatus.TAKEOUT) {
-        const nextBtn = getNextBtn();
+        const nextBtn = await waitForElementWithTextContent("button", "Next", 1000);
         if (!nextBtn) return;
 
-        let startSec = nextPlayerOnTakeOutStuck.sec;
+        let startSec = configValue.nextPlayerOnTakeOutStuck.sec;
 
         const nextBtnTextEl = document.createElement("span");
         nextBtnTextEl.id = "ad-ext_next-leg-text";
@@ -30,9 +75,15 @@ export async function nextPlayerOnTakeOutStuck() {
           nextBtnTextEl.textContent = ` (${startSec})`;
 
           if (startSec <= 0) {
-            clearInterval(takeOutTimout);
-            (nextBtn as HTMLElement).click();
-            document.getElementById("ad-ext_next-leg-text")?.remove();
+            if (takeOutTimout) {
+              nextBtnTextEl.textContent = ""; // Reset the button text
+              clearInterval(takeOutTimout);
+            }
+            if (nextBtn instanceof HTMLElement) {
+              nextBtn.click();
+            }
+            const element = document.getElementById("ad-ext_next-leg-text");
+            element?.remove();
           }
         }, 1000);
       }
